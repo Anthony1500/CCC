56708d1eaff35628f4e978a5036846999ed543c7
Anthony <arteaga17000@gmail.com>
2023-04-25 19:37:10 -0500

    v0.1

---
+++ Proyecto/src/General/Ejecutar.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package General;
+
+/**
+ * @author German_Arteaga
+ *
+ */
+public class Ejecutar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a=4;
+		int b=6;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
---
+++ Proyecto/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author German Arteaga
+ *
+ */
+module ejecutar {
+}
